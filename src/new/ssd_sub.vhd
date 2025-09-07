991e7fcc8026c601e8e3146915df24c6bffaceea
ulassakin <mulassakin@gmail.com>
2025-09-07 10:29:16 +0300

    ssd sub module updated

--- src/new/ssd_sub.vhd
+++ src/new/ssd_sub.vhd
@@ -1,21 +1,21 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.ALL; -- To have std_logic and std_logic_vector types
 
 
-entity ssd_sub is
-    Port ( bcd_i : in STD_LOGIC_VECTOR(3 downto 0);
-           ssd_o : out STD_LOGIC_VECTOR(7 downto 0));
+entity ssd_sub is -- Define inputs and outputs
+    Port ( bcd_i : in STD_LOGIC_VECTOR(3 downto 0);-- bcd input
+           ssd_o : out STD_LOGIC_VECTOR(7 downto 0));-- ssd output
 end ssd_sub;
 
 architecture Behavioral of ssd_sub is
 
 begin
 
-process (bcd_i) 
+process (bcd_i) -- All lines work sequentially in here. It triggers when bcd_i is changed
 begin
     
        
-    case bcd_i is 
+    case bcd_i is -- Change output according to what input is
         
         when "0000" => ssd_o <= "00000011";--deciding which cathodes will ligth up
         when "0001" => ssd_o <= "10011111";--according to bcd we got from multiplier
